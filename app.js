658bb5f565a8b04212f3ea8faabc351c97956516
DoubleB123 <bbrenner321@gmail.com>
2018-05-02 18:07:51 -0700

    inital commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+// setup
+const UPLOAD_PATH = 'uploads';
+
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage, limits: {fileSize: 1000*1000*50}}); // multer configuration
+
+// app
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/html', 'index.html')));
+
+app.post('/fileupload', upload.single('testfile'), (req, res) => {
+  const file = req.file;
+  console.log('request to upload');
+  console.log(file);
+  res.sendStatus(200);
+});
+
+
+const portNum = 3001;
+const server = app.listen(portNum, '0.0.0.0', () => {
+  console.log('Server running. Connect to http://localhost:' + portNum)
+})
+
+
+module.exports = app;
